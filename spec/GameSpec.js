43e3fbfc6cf5a05d6fe61173e1c07df69de749b5
Phil Smithies <philsmithies@gmail.com>
2021-05-08 15:42:02 +0200

    spares added

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,5 +1,5 @@
 describe('Game', function() {
-  let bowling;
+  let game;
 
   beforeEach(function(){
     game = new Game;
@@ -16,6 +16,22 @@ describe('Game', function() {
   it('Can push two values into one frame', () => {
     game.add(1);
     game.add(2);
-    expect(game.frames).toEqual([ [ 1 ], [ 2 ] ] );
+    expect(game.frames).toEqual([ 1, 2 ]);
   });
+  it('Will reset the frame after a round', () => {
+    game.add(1);
+    game.add(2);
+    expect(game.frame).toEqual(1);
+  });
+  // it('Will not continue adding once the game has ended', () =>{
+  //   game.round = 11
+  //   expect(game.add(2)).toEqual(null);
+  // });
+  it('Will output strike when the user throws a 10', () =>{
+    expect(game.add(10)).toEqual('strike');
+  })
+  it('Will output spare when the user throws a 10 between the first and second throws', () =>{
+    game.add(2)
+    expect(game.add(8)).toEqual('spare');
+  })
 });
--- src/Game.js
+++ src/Game.js
@@ -6,21 +6,43 @@ class Game {
     this.frames = []
     this.round = 0
     this.frame = 1
+    this.STRIKE_SCORE = 10
+    this.roundScore = []
   }
 
   add(score){
-    let roundScore = []
-    if (this.frame == 1) {
-      roundScore.push(score);
-      this.frames.push(roundScore);
-      this.frame += 1
+    console.log(this.round)
+
+    if (this.gameOver()) {
+      return null
+
+    } else if (this.frame == 1) {
+
+      if (score == 10) {
+        this.frames.push(this.STRIKE_SCORE);
+        this.frame == 1
+        this.round += 1
+        return('strike')
+      }
+
+      this.frames.push(score);
+      this.advFrame()
+    
     } else if (this.frame == 2) {
-      roundScore.push(score);
-      this.frames.push(roundScore);
-      this.resetFrame()
+
+      if (score + this.frames[this.frames.length - 1] == 10){
+        return('spare')
+      }
+
+      this.frames.push(score);
+      this.frame = 1;
+      this.roundScore = []
     }
-    roundScore = []
+
+    this.round += 1
+    return score
   };
+
 
   scoreTotal() {
     let newArr = this.frames.flat();
@@ -28,23 +50,23 @@ class Game {
       return a + b}, 0)
   };
 
-  // _isAStrike(){
-  //   if(this.frame = 1 && this.add() == 10) {
-
-  //   }      
-  // }
-
-  // lastFrame(){
-  //   return true
-  // }
+  gameOver(){
+    if (this.round == 11) {
+      return true
+    }
+  }
 
   resetFrame() {
-    this.frame == 0
-
+    this.frame == 1
   }
 
   advFrame() {
     this.frame += 1
   }
 
+  isStrike(){
+    if (this.add.roundScore == [10]) {
+      console.log("strike")
+    }
+  }
   }
