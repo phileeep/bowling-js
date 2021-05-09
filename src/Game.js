e9a1643ccc7b90ee98d7802bd73ab59f8c9b97ea
Phil Smithies <philsmithies@gmail.com>
2021-05-09 11:16:12 +0200

    gameover function

--- src/Game.js
+++ src/Game.js
@@ -3,10 +3,20 @@
 class Game {
 
   constructor() {
-
+    score = new ScoreCalculator();
   }
 
   move(roll){
+    score.add(roll)
+  }
+
+  isGutterGame(){
+    if (score.addedScores() == 0){
+      return true
+    }
+  }
+
+  isPerfectGame() {
     
   }
 
